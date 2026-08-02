18d022db20aa15b24d80f956a0f2068649b3f525
robiulsagor <robiul100.me@gmail.com>
2026-08-02 10:39:57 +0600

    feat: implement login form with validation and error handling

--- app/(auth)/login/login-form.tsx
+++ app/(auth)/login/login-form.tsx
@@ -1,25 +1,73 @@
+"use client";
+
+import TitleCard from "@/components/shared/auth/title-card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
+import { LoginFormData, loginSchema } from "@/lib/validations/auth";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { useForm, SubmitHandler } from "react-hook-form";
 
 const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormData>({
+    resolver: zodResolver(loginSchema),
+  });
+
+  const onSubmit: SubmitHandler<LoginFormData> = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className=" w-full flex flex-col lg:flex-row p-1 lg:p-10 gap-5">
-      <div className="flex-1 flex items-center justify-center flex-col space-y-2 min-h-30">
-        <p className="text-2xl  font-bold text-slate-700">Expense Tracker</p>
-        <p className=" text-slate-500">Login to your account</p>
-      </div>
+      <TitleCard subtitle="Login to your account" />
 
-      <form action="" className="flex-1 space-y-4">
-        <Input type="email" placeholder="Email" className="w-full" />
-        <Input type="password" placeholder="Password" className="w-full" />
-        <Button className="bg-blue-500 text-white px-4 py-2 rounded-md w-full">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex-1 space-y-4">
+        <div>
+          <Input
+            type="email"
+            placeholder="Email"
+            className="w-full"
+            {...register("email", { required: true })}
+          />
+          {errors.email && (
+            <span className="text-red-500 text-xs">{errors.email.message}</span>
+          )}
+        </div>
+
+        <div>
+          <Input
+            type="password"
+            placeholder="Password"
+            className="w-full"
+            {...register("password", { required: true })}
+          />
+          {errors.password && (
+            <span className="text-red-500 text-xs">{errors.password.message}</span>
+          )}
+        </div>
+
+        <Button
+          type="submit"
+          className="bg-blue-500 text-white px-4 py-2 rounded-md w-full"
+        >
           Login
         </Button>
         <div className="flex flex-col space-y-2 text-sm text-slate-500">
-
-        <p>Don&apos;t have an account? <Link href="/signup" className="text-blue-500">Sign up</Link></p>
-        <p>Forgot your password? <Link href="/reset-password" className="text-blue-500">Reset it</Link></p>
+          <p>
+            Don&apos;t have an account?{" "}
+            <Link href="/register" className="text-blue-500">
+              Register
+            </Link>
+          </p>
+          <p>
+            Forgot your password?{" "}
+            <span className="text-blue-500">Reset it</span>
+          </p>
         </div>
       </form>
     </div>
