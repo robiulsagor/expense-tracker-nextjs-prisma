01162ddcdbca252a1c846027ca31e5a78dd12a19
robiulsagor <robiul100.me@gmail.com>
2026-08-02 20:31:19 +0600

    feat: add MonthSelector component for navigating between months

---
+++ components/shared/MonthSelector.tsx
@@ -0,0 +1,57 @@
+import { formatMonth } from "@/lib/helper";
+import { ChevronLeft } from "lucide-react";
+
+const MonthSelector = ({
+  currentMonth,
+  setCurrentMonth,
+}: {
+  currentMonth: Date;
+  setCurrentMonth: React.Dispatch<React.SetStateAction<Date>>;
+}) => {
+  const handlePrevMonth = () => {
+    setCurrentMonth((prev) => {
+      const date = new Date(prev);
+      date.setMonth(date.getMonth() - 1);
+
+      return date;
+    });
+  };
+  const isNextMonthDisabled =
+    currentMonth.getMonth() === new Date().getMonth() &&
+    currentMonth.getFullYear() === new Date().getFullYear();
+
+  const handlenextMonth = () => {
+    if (isNextMonthDisabled) return; // Prevent going to the next month if it's the current month
+
+    setCurrentMonth((prev) => {
+      const date = new Date(prev);
+      date.setMonth(date.getMonth() + 1);
+      return date;
+    });
+  };
+
+  return (
+    <div className="flex gap-6 items-center justify-center text-lg mb-3">
+      <button
+        className="border border-slate-400 hover:bg-slate-200 transition p-1 rounded cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-transparent *:active:scale-90 active:scale-90 disabled:active:scale-100"
+        onClick={handlePrevMonth}
+      >
+        <ChevronLeft size={20} className="text-slate-600 " />
+      </button>
+
+      <p className="text-slate-500 min-w-50 text-center">
+        {formatMonth(currentMonth)}
+      </p>
+
+      <button
+        className="border border-slate-400 hover:bg-slate-200 transition p-1 rounded cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-transparent active:scale-90 disabled:active:scale-100"
+        onClick={handlenextMonth}
+        disabled={isNextMonthDisabled}
+      >
+        <ChevronLeft size={20} className="text-slate-600 rotate-180" />
+      </button>
+    </div>
+  );
+};
+
+export default MonthSelector;
