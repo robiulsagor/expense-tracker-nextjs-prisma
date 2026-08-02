3416f23c724c9906da3a6cc755668a5315a99266
robiulsagor <robiul100.me@gmail.com>
2026-08-02 17:45:22 +0600

    fix: adjust padding in Navbar component

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -2,7 +2,7 @@ import { UserCircleIcon } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <div className="flex items-center justify-between p-4  border-b border-slate-300">
+    <div className="flex items-center justify-between py-4 border-b border-slate-300">
       <div>
         <h2 className="text-2xl font-bold text-slate-700">Expense Tracker</h2>
         <span className="text-sm text-slate-500">
