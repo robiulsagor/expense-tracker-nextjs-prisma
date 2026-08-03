eee474c48016e45fd54a96ce0e3e2422c486916c
robiulsagor <robiul100.me@gmail.com>
2026-08-03 22:19:35 +0600

    style: update HeroCards component to allow flexible card layout with wrapping

--- components/shared/cards/HeroCards.tsx
+++ components/shared/cards/HeroCards.tsx
@@ -11,7 +11,7 @@ const HeroCards = () => {
     <div className=" bg-slate-50 p-6 rounded-lg shadow-md">
       <MonthSelector currentMonth={currentMonth} setCurrentMonth={setCurrentMonth} />
 
-      <div className=" flex gap-6">
+      <div className=" flex flex-wrap gap-6">
         <SingleCard amount={1000} type="income" />
         <SingleCard amount={500} type="expense" />
         <SingleCard amount={500} type="balance" />
