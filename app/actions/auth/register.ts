7a4525f89f97c96d0c2746a8bf9450c0de8064d1
robiulsagor <robiul100.me@gmail.com>
2026-08-07 01:28:39 +0600

    feat: enhance user registration by adding sign-in functionality after successful registration

--- app/actions/auth/register.ts
+++ app/actions/auth/register.ts
@@ -1,8 +1,10 @@
 "use server"
+import { isRedirectError } from "next/dist/client/components/redirect-error";
 
 import bcrypt from "bcrypt";
 import { createUser, getUserByEmail } from "@/lib/queries/user";
 import { RegisterFormData } from "@/lib/validations/auth";
+import { signIn } from "@/auth";
 
 export const registerUser = async (data: RegisterFormData) => {
     try{
@@ -24,12 +26,23 @@ export const registerUser = async (data: RegisterFormData) => {
   const hashedPassword = await bcrypt.hash(data.password, 10);
   await createUser(data.name, data.email, hashedPassword);
 
+  await signIn("credentials", {
+    email: data.email,
+    password: data.password,
+    redirectTo: "/",
+  });
+
   return {
     success: true,
     message: "User registered successfully",
   };
 } catch (error) {
     console.error("Error registering user:", error);
+     if (isRedirectError(error)) {
+    throw error;
+  }
+
+  console.error(error);
 
     return {
       success: false,
