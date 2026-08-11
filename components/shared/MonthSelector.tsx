cd42f800f6f0930a68509aaa8ee7cc54dea52ade
robiulsagor <robiul100.me@gmail.com>
2026-08-11 18:56:35 +0600

    feat: refactor MonthSelector to manage month state internally and remove props from HeroCards

--- components/shared/MonthSelector.tsx
+++ components/shared/MonthSelector.tsx
@@ -1,37 +1,51 @@
+"use client";
+
 import { formatMonth } from "@/lib/helper";
 import { ChevronLeft } from "lucide-react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useTransition } from "react";
 
-const MonthSelector = ({
-  currentMonth,
-  setCurrentMonth,
-}: {
-  currentMonth: Date;
-  setCurrentMonth: React.Dispatch<React.SetStateAction<Date>>;
-}) => {
+const MonthSelector = () => {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const searchParams = useSearchParams();
+
+  const month = Number(searchParams.get("month")) || new Date().getMonth();
+  const year = Number(searchParams.get("year")) || new Date().getFullYear();
+
+  const date = new Date(year, month, 1);
+
   const handlePrevMonth = () => {
-    setCurrentMonth((prev) => {
-      const date = new Date(prev);
-      date.setMonth(date.getMonth() - 1);
+    const newDate = date.getMonth() - 1;
 
-      return date;
+    startTransition(() => {
+      router.push(`?month=${newDate}&year=${date.getFullYear()}`);
     });
   };
   const isNextMonthDisabled =
-    currentMonth.getMonth() === new Date().getMonth() &&
-    currentMonth.getFullYear() === new Date().getFullYear();
+    date.getMonth() - 1 === new Date().getMonth() &&
+    date.getFullYear() === new Date().getFullYear();
 
   const handlenextMonth = () => {
     if (isNextMonthDisabled) return; // Prevent going to the next month if it's the current month
 
-    setCurrentMonth((prev) => {
-      const date = new Date(prev);
-      date.setMonth(date.getMonth() + 1);
-      return date;
+    const newDate = date.getMonth() + 1;
+    startTransition(() => {
+      router.push(`?month=${newDate}&year=${date.getFullYear()}`);
     });
   };
 
+  // if there is no month and year in the search params, set it to the current month and year
+  useEffect(() => {
+    startTransition(() => {
+      router.push(`?month=${date.getMonth()}&year=${date.getFullYear()}`);
+    });
+  }, []);
+
   return (
-    <div className="flex gap-6 items-center justify-center text-lg mb-3">
+    <div
+      className={`flex gap-6 items-center justify-center text-lg mb-3 ${isPending ? "opacity-50" : "opacity-100"}`}
+    >
       <button
         className="border border-slate-400 hover:bg-slate-200 transition p-1 rounded cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-transparent *:active:scale-90 active:scale-90 disabled:active:scale-100"
         onClick={handlePrevMonth}
@@ -40,8 +54,10 @@ const MonthSelector = ({
         <ChevronLeft size={20} className="text-slate-600 " />
       </button>
 
-      <p className="text-slate-500 min-w-40 text-center">
-        {formatMonth(currentMonth)}
+      <p
+        className={`min-w-40 text-center ${isPending ? "text-slate-300" : "text-slate-500"}`}
+      >
+        {formatMonth(date)}
       </p>
 
       <button
--- components/shared/cards/HeroCards.tsx
+++ components/shared/cards/HeroCards.tsx
@@ -1,30 +1,14 @@
-"use client";
-
-import React, { useEffect, useState } from "react";
 import SingleCard from "./SingleCard";
 import MonthSelector from "../MonthSelector";
-import { useRouter } from "next/navigation";
 
 const HeroCards = ({
   summary,
 }: {
   summary: { income: number; expense: number };
 }) => {
-  const router = useRouter();
-  const [currentMonth, setCurrentMonth] = useState(new Date());
-
-  useEffect(() => {
-    router.push(
-      `?month=${currentMonth.getMonth() + 1}&year=${currentMonth.getFullYear()}`,
-    );
-  }, [currentMonth, router]);
-
   return (
     <div className=" bg-slate-50 p-6 rounded-lg shadow-md">
-      <MonthSelector
-        currentMonth={currentMonth}
-        setCurrentMonth={setCurrentMonth}
-      />
+      <MonthSelector />
 
       <div className="flex flex-wrap gap-6">
         <SingleCard amount={summary.income} type="income" />
